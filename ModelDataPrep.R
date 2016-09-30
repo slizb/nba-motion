2b000c1002f059c1e2f1f2b3e4fdeca0219515b5
slizb <bradsliz@gmail.com>
2016-09-29 20:47:57 -0500

    added balanced train set, converted categoricals to factor, dropped un-needed columns

--- ModelDataPrep.R
+++ ModelDataPrep.R
@@ -1,39 +1,33 @@
 
 library(dplyr)
 library(caret)
+library(readr)
+
+rm(list=ls())
 
 set.seed(321)
 
-threes <- read.csv('/volumes/nba/threes.csv')
-
-feats <- read.csv('event_features_7-17-16.csv')
-
-df <- threes %>% 
-     left_join(feats, 
-               by = c('gameid' = 'gameid',
-                      'event.id' = 'event.id')) %>% 
+df <- read.csv('event_features_9-20-16.csv') %>% 
      .[complete.cases(.),]
 
+
 model_df <- df %>% 
-     mutate(target = ifelse(EVENTMSGTYPE == 1, 'Make', 'Miss')) %>% 
-     select(-c(lastnameshooter, 
-               teamName,
-               id,
-               EVENTNUM,
-               EVENTMSGTYPE,
-               EVENTMSGACTIONTYPE,
-               event.id,
-               PLAYER1_ID,
-               gameid,
-               team_off,
-               team_def))
+     mutate(target = ifelse(EVENTMSGTYPE == 1, 'Make', 'Miss'),
+            target = as.factor(target),
+            team_id = as.factor(team_id),
+            PLAYER1_ID = as.factor(PLAYER1_ID)) %>% 
+     select(-c(X,
+               EVENTMSGTYPE))
 
 trainIndex <- createDataPartition(model_df$target, 
-                                  p = .6,
-                                  list = FALSE,
+                                  p = .7,
+                                  list = F,
                                   times = 1)
 
 train <- model_df[ trainIndex,]
 test  <- model_df[-trainIndex,]
 
-save(train, test, file = "preppedData.RData")
+up_train <- upSample(x = train %>% select(-target),
+                     y = train$target)
+
+save(train, test, up_train, file = "preppedData.RData")
